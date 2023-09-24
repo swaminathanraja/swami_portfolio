589483c8a53ad6f97303c75ee713ba1c570c3204
swaminathanraja <131260489+swaminathanraja@users.noreply.github.com>
2023-09-24 12:34:17 +0530

    Add files via upload
    basic portfolio code using html , css and js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+
+    });
+
+    $(window).on('scroll load',function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+
+        if($(window).scrollTop() > 0){
+            $('.top').show();
+        }else{
+            $('.top').hide();
+        }
+
+    });
+
+    $('a[href*="#"]').on('click',function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop : $($(this).attr('href')).offset().top,
+        },
+        500,
+        'linear'
+        );
+
+    });
+
+});
